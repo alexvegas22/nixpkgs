9a23ff1224117376d1eff8c6d5c48f0bb3ee5192
Alex <67081273+alexvegas22@users.noreply.github.com>
2025-01-02 02:55:17 -0500

    Update signal-desktop.nix 7.36.0 -> 7.36.1

--- pkgs/by-name/si/signal-desktop/signal-desktop.nix
+++ pkgs/by-name/si/signal-desktop/signal-desktop.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "7.36.0";
+  version = "7.36.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-5p9Vnxj53jOZbEirWamwv4Fkm/fMLeLfV93GDrV8XuA=";
+  hash = "sha256-dfa54daf8015b62e074f07601ee1d71b8e2222126886238263d233bd941131cd";
 }
