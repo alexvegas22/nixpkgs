50b569adcde22a274ebf50878f4ed8f618c7a397
Alex <67081273+alexvegas22@users.noreply.github.com>
2025-01-02 03:01:06 -0500

    corrected hash

--- pkgs/by-name/si/signal-desktop/signal-desktop.nix
+++ pkgs/by-name/si/signal-desktop/signal-desktop.nix
@@ -4,5 +4,5 @@ callPackage ./generic.nix { } rec {
   dir = "Signal";
   version = "7.36.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-dfa54daf8015b62e074f07601ee1d71b8e2222126886238263d233bd941131cd";
+  hash = "sha256-36VNr4AVti4HTwdgHuHXG44iIhJohiOCY9IzvZQRMc0=";
 }
